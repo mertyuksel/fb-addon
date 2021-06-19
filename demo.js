d9709fbc9a7ce8918d4962465f4c98e3368c3477
mertyuksel <51139965+mertyuksel@users.noreply.github.com>
2021-06-19 15:47:16 +0300

    just testing

--- demo.js
+++ demo.js
@@ -1,6 +1,5 @@
 
-/******************************************************************/
-
+/******************** REQUIRED *******************/
 // required 
 const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1c7lU83nFRCi6GbSZGWR1nTARlzV3KLvW4-lW7UNJ2hM/edit#gid=0'   
 
@@ -13,6 +12,8 @@ const ACCOUNT_ID = '4653630351333052'
 // required
 const TOKEN = 'EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU'
 
+
+/******************** OPTIONAL *******************/
 // optional
 const LEVEL = ''
 
@@ -26,7 +27,7 @@ const DATE_PRESET = ''
 const TIME_INCREMENT = ''
 
 // optional
-const FILTERING = ''  // "[{'field':'action_type','operator':'IN','value':['link_click']}]"
+const FILTERING = ''  
 
 // optional  
 const BREAKDOWNS = '' 
@@ -34,7 +35,7 @@ const BREAKDOWNS = ''
 
 function getFacebookAdsData() {
 
-  const baseUrl = `https://graph.facebook.com/v10.0/act_${ACCOUNT_ID}/insights?level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_PRESET}&access_token=${TOKEN}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&limit=500`
+  const baseUrl = `https://graph.facebook.com/v10.0/act_${ACCOUNT_ID}/insights?access_token=${TOKEN}&level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_PRESET}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&breakdowns=${BREAKDOWNS}&limit=500`
     const encodedUrl = encodeURI(baseUrl);  
 
   var response = UrlFetchApp.fetch(encodedUrl, {'method' : 'post'});  
@@ -57,62 +58,20 @@ function getFacebookAdsData() {
     checkResponse = JSON.parse(checkAsync.getContentText());
   }
 
-
-/*
-// PART1  
+  const reportUrl = `https://www.facebook.com/ads/ads_insights/export_report?report_run_id=${REPORT_ID}&format=csv&access_token=${TOKEN}`;
+  const csv = UrlFetchApp.fetch(reportUrl);  
+  const csvData  = Utilities.parseCsv(csv);
   
-  const insightUrl = `https://graph.facebook.com/v10.0/${REPORT_ID}/insights?access_token=${TOKEN}&export_format=csv`;
-  rawAdsData = UrlFetchApp.fetch(insightUrl);
 
-  // parser calismasi icin veri bu kivama gelmeli "a,b,c\nd,e,f" 
-  // bir sonraki adim -- bu bizim adamin ki nasil calisiyor csv olarak export edeni diyorum bi ona bak bakalim. calistir en bastan.
+  csvData.length // 2
+ 
 
-  // json dosyasindan json movie data okudum ama parseCsv() onu parse etmedi. 
-  var csvData = Utilities.parseCsv(rawAdsData);  
-  sheet.getRange(1, 1, csvData.length, csvData[0].length).setValues(csvData);
-*/
-/*
-{
-   "data": [
-      
-   ],
-   "paging": {
-      "cursors": {
-         "before": "MAZDZD",
-         "after": "LTEZD"
-      }
-   }
-}
-
-PARSING HATASI VERIYOR. YUKARIDAKI LINKIN OUTPUT'U 
-export_format=csv BUNU BUNA CEVIRDIM format=csv HALA DOSYA DONDURMUYOR LINK   = graph api da bu ozellik yokmus kankam export_format yani. 
-TODO: yukaridaki linkin output alma parametrelerine dokumantasyondan bir daha kontrol et nedense alttaki gibi csv dondurmuyor. 
-
-// graph api export olayina sahip degilmis -- bu link degerli
-// https://stackoverflow.com/questions/48410866/in-the-fb-marketing-api-there-is-an-option-to-export-format-csv-how-do-i-do 
-
-*/
+  Logger.log(csvData); // 	[[], [No data available.]]
 
 
-
-  // PART2
-  // Olmadi direkt bununla birlikte bi yayinlariz. 
-  // Fetches the report as a csv file
-  const url = `https://www.facebook.com/ads/ads_insights/export_report?report_run_id=${REPORT_ID}&format=csv&access_token=${TOKEN}`;
-  const fetchRequest = UrlFetchApp.fetch(url);  // .getContentText(); bu ne is yapiyor bunu dene belki bundan olmuyor yukarida. 
-  Logger.log(fetchRequest.getContentText);
-  const results = Utilities.parseCsv(fetchRequest);
-  
-  // Pastes the csv file in the sheet
-  sheet.getRange(1,1, results.length, results[0].length).setValues(results);
-
-
-/*
-
-OUTPUT DIREKT OLARAK CSV DOSTUM BURADA --- VE ICINDE DIREKT OLARAK "NO DATA AVAILABLE" YAZIYOR 
-*/
-
+  // csvData.length = 2 CIKIYOR. 
+  sheet.getRange(1,1, csvData.length, csvData[0].length).setValues(csvData);
 
   Logger.log(checkResponse.async_status)
-
 }
+
