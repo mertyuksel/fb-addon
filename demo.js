ae9179245dda808b70c9613d796aaf8978cb70d6
mertyuksel <51139965+mertyuksel@users.noreply.github.com>
2021-06-19 10:14:32 +0300

    ready to launch 3

--- demo.js
+++ demo.js
@@ -1,141 +1,118 @@
 
-const SPREADSHEET_URL = ''
+/******************************************************************/
 
-const TAB_NAME = ''
+// required 
+const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1c7lU83nFRCi6GbSZGWR1nTARlzV3KLvW4-lW7UNJ2hM/edit#gid=0'   
 
-const ACCOUNT_ID = ''
+// required 
+const TAB_NAME = 'Sheet1'
 
-const TOKEN = ''
+// required
+const ACCOUNT_ID = '4653630351333052'
 
+// required
+const TOKEN = 'EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU'
+
+// optional
 const LEVEL = ''
 
+// optional
 const FIELDS = ''
 
+// optional
 const DATE_PRESET = ''
 
+// optional
 const TIME_INCREMENT = ''
 
-const FILTERING = ''  
+// optional
+const FILTERING = ''  // "[{'field':'action_type','operator':'IN','value':['link_click']}]"
 
-// TODO: encoding islemine ihtiyac duyuyor. birde encode etmeden dene bakalim encodeURI kullanmadan.
-// filtering kismi kesinlkle encoding ihtiyac duyuyor parantez ve tirnak isareti kisimlari gibi. 
-// eger yapmaz isen UrlFetch calismiyor. invalid argument oluyor. 
+// optional  
+const BREAKDOWNS = '' 
 
-// TODO: parametrenin value kismi eksik oldugunda ne olur. teklide bu yasaniyor.  TEST ET. 
 
 function getFacebookAdsData() {
 
   const baseUrl = `https://graph.facebook.com/v10.0/act_${ACCOUNT_ID}/insights?level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_PRESET}&access_token=${TOKEN}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&limit=500`
-  
-  // filtering kismi icin gerekli. 
-  const encodedUrl = encodeURI(baseUrl);  
-  
-  // TODO: output incele gercekten encode ediyor mu nasil ediyor bak.
-  Logger.log(encodedUrl) 
+    const encodedUrl = encodeURI(baseUrl);  
 
   var response = UrlFetchApp.fetch(encodedUrl, {'method' : 'post'});  
   const data = JSON.parse(response.getContentText());
   const REPORT_ID = data.report_run_id;
-
-  const spreadSheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
-  const sheet = spreadSheet.getSheetByName(TAB_NAME);
-  
-  sheet.clear();
-  
-  // ENCODING GEREKSIZ DENEDIM. 
+ 
   const checkUrl = `https://graph.facebook.com/v10.0/${REPORT_ID}?access_token=${TOKEN}`;
   var checkAsync = UrlFetchApp.fetch(checkUrl);
   var checkResponse = JSON.parse(checkAsync.getContentText());
 
+  const spreadSheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  const sheet = spreadSheet.getSheetByName(TAB_NAME);
+  sheet.clear();
+
   while (checkResponse.async_status != "Job Completed")
   {
-    Logger.log(checkResponseresponse.async_status)
+    Logger.log(checkResponse.async_status)
     Utilities.sleep(2000);
-    checkAsync = UrlFetchApp.fetch(url);
+    checkAsync = UrlFetchApp.fetch(checkUrl);
     checkResponse = JSON.parse(checkAsync.getContentText());
   }
 
-  // ENCODING GEREKSIZ DENEDIM. 
+
+/*
+// PART1  
+  
   const insightUrl = `https://graph.facebook.com/v10.0/${REPORT_ID}/insights?access_token=${TOKEN}&export_format=csv`;
   rawAdsData = UrlFetchApp.fetch(insightUrl);
 
+  // parser calismasi icin veri bu kivama gelmeli "a,b,c\nd,e,f" 
+  // bir sonraki adim -- bu bizim adamin ki nasil calisiyor csv olarak export edeni diyorum bi ona bak bakalim. calistir en bastan.
+
+  // json dosyasindan json movie data okudum ama parseCsv() onu parse etmedi. 
   var csvData = Utilities.parseCsv(rawAdsData);  
   sheet.getRange(1, 1, csvData.length, csvData[0].length).setValues(csvData);
-
-  Logger.log(results.async_status)
+*/
+/*
+{
+   "data": [
+      
+   ],
+   "paging": {
+      "cursors": {
+         "before": "MAZDZD",
+         "after": "LTEZD"
+      }
+   }
 }
 
+PARSING HATASI VERIYOR. YUKARIDAKI LINKIN OUTPUT'U 
+export_format=csv BUNU BUNA CEVIRDIM format=csv HALA DOSYA DONDURMUYOR LINK   = graph api da bu ozellik yokmus kankam export_format yani. 
+TODO: yukaridaki linkin output alma parametrelerine dokumantasyondan bir daha kontrol et nedense alttaki gibi csv dondurmuyor. 
 
-/*
-
-const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1c7lU83nFRCi6GbSZGWR1nTARlzV3KLvW4-lW7UNJ2hM/edit#gid=0'
-
-const TAB_NAME = 'Sheet1'
-
-const TOKEN = 'EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU'
-
-const COMPLETE_URL = 'https://graph.facebook.com/v10.0/act_4653630351333052/insights?level=campaign&export_format=xls&fields=impressions&date_preset=yesterday&access_token=EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU&time_increment=1&filtering=[{field:"action_type",operator:"IN",value:[\'mobile_app_install\']}]&limit=500'
-
-https://graph.facebook.com/v10.0/act_4653630351333052/insights?level=campaign&export_format=xls&fields=impressions&date_preset=yesterday&access_token=EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU&time_increment=1&filtering=[{field:%22action_type%22,operator:%22IN%22,value:[\%27mobile_app_install\%27]}]&limit=500
-
-
-
-
-
-
-var REPORT_ID; 
-
-
-// put your complete URL you've already prepared 
-const COMPLETE_URL = 'COMPLETE_URL'
-
-
-
-// complete spreadsheet URL 
-const SPREADSHEET_URL = 'SPREADSHEET_URL'
-
-// spreadsheet tab name (like Sheet1)
-const TAB_NAME = 'TAB_NAME'
-
-// facebook ad account ID 
-const ACCOUNT_ID = 'ACCOUNT_ID'
-
-// user access token from the facebook app you've created
-const TOKEN = 'TOKEN'
+// graph api export olayina sahip degilmis -- bu link degerli
+// https://stackoverflow.com/questions/48410866/in-the-fb-marketing-api-there-is-an-option-to-export-format-csv-how-do-i-do 
 
 */
 
 
 
-/*
-
-'{"data": [ { "event_name": "Purchase", "event_time": 1622209315, "user_data": { "em": [ "309a0a5c3e211326ae75ca18196d301a9bdbd1a882a4d2569511033da23f0abd" ], "ph": [ "254aa248acb47dd654ca3ea53f48c2c26d641d23d7e2e93a1ec56258df7674c4", "6f4fcb9deaeadc8f9746ae76d97ce1239e98b404efe5da3ee0b7149740f89ad6" ], "client_ip_address": "123.123.123.123", "client_user_agent": "$CLIENT_USER_AGENT", "fbc": "fb.1.1554763741205.AbCdEfGhIjKlMnOpQrStUvWxYz1234567890", "fbp": "fb.1.1558571054389.1098115397" }, "contents": [ { "id": "product123", "quantity": 1, "delivery_category": "home_delivery" } ], "custom_data": { "currency": "usd", "value": 123.45 }, "event_source_url": "http://jaspers-market.com/product/123", "action_source": "website" } ]}'
-
-*/
-
-/*
-
-asyn job sonucu ogrenmek icin link:
-
-https://graph.facebook.com/v10.0/478204073432587/insights?access_token=EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU
-
-
-https://graph.facebook.com/v10.0/1670232720033688?access_token=EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU
-
-*/
-
+  // PART2
+  // Olmadi direkt bununla birlikte bi yayinlariz. 
+  // Fetches the report as a csv file
+  const url = `https://www.facebook.com/ads/ads_insights/export_report?report_run_id=${REPORT_ID}&format=csv&access_token=${TOKEN}`;
+  const fetchRequest = UrlFetchApp.fetch(url);  // .getContentText(); bu ne is yapiyor bunu dene belki bundan olmuyor yukarida. 
+  Logger.log(fetchRequest.getContentText);
+  const results = Utilities.parseCsv(fetchRequest);
+  
+  // Pastes the csv file in the sheet
+  sheet.getRange(1,1, results.length, results[0].length).setValues(results);
 
 
 /*
 
-https://graph.facebook.com/v10.0/act_4653630351333052/insights?level=campaign&export_format=xls&fields=impressions&date_preset=yesterday&access_token=EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU&time_increment=1&filtering=[{field:"action_type",operator:"IN",value:['mobile_app_install']}]&limit=1000
-
+OUTPUT DIREKT OLARAK CSV DOSTUM BURADA --- VE ICINDE DIREKT OLARAK "NO DATA AVAILABLE" YAZIYOR 
 */
 
 
-// parametrelerin sag tarafi bos olmali mert -- biri doldurmadan fetch yaptiginda hata vermesin diye. 
+  Logger.log(checkResponse.async_status)
 
-// her parametre ayri sekilde olucak hepsinin detayli anlatimi websitesinde olucak merak etme burada link aciklama vs vermeyeceksin. 
-// degisken isimlerini degistirmeyi unutma. 
-
-
+}
