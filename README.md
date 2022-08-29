6113dbbf1abfb66cb77ae6aa3e01a4a29383a822
mertyuksel <51139965+mertyuksel@users.noreply.github.com>
2022-08-29 16:37:21 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# data transfer script 
+
+A script that extracts Facebook ads data to Google Sheets.
+
+See the [blog post]() to learn how to utilize the script effectively.
+
+
+
+
--- demo.js
+++ demo.js
@@ -1,41 +1,31 @@
 /******************** REQUIRED *******************/
 
-// required 
-const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1c7lU83nFRCi6GbSZGWR1nTARlzV3KLvW4-lW7UNJ2hM/edit#gid=0'   
+const SPREADSHEET_URL = ''   
 
-// required 
-const SHEET_TAB_NAME = 'temp'
+const SHEET_TAB_NAME = ''
 
-// required
-const ACCOUNT_ID = '4653630351333052'
+const ACCOUNT_ID = ''
 
-// required
-const TOKEN = 'EAAExgau2As4BAKgfoPN84jbun6jUJM7Vd9cOvjyUbhj610ZB2H1ZCgrouYEcZBl58QmYNZA3TqR1JkQ25CzC7O7ZAYH5Dw8LJHpdiTkfRbZB3dNZCPasEVZCx4EWamIQt7aJUyWHGo6NMCsFyDNiK7AeqfODI5WZBd9L9O4sCZCmBRSPvAj0ATKxaU'
+const TOKEN = ''
 
 /******************** OPTIONAL *******************/
 
-// optional
 const LEVEL = ''
 
-// optional
 const FIELDS = ''
 
-// optional
 const DATE_PRESET = ''
 
-// optional
 const TIME_INCREMENT = ''
 
-// optional
 const FILTERING = ''  
 
-// optional  
 const BREAKDOWNS = '' 
 
 
 function getFacebookAdsData() {
 
-  const baseUrl = `https://graph.facebook.com/v10.0/act_${ACCOUNT_ID}/insights?access_token=${TOKEN}&level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_PRESET}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&breakdowns=${BREAKDOWNS}&limit=500`
+  const baseUrl = `https://graph.facebook.com/v14.0/act_${ACCOUNT_ID}/insights?access_token=${TOKEN}&level=${LEVEL}&fields=${FIELDS}&date_preset=${DATE_PRESET}&time_increment=${TIME_INCREMENT}&filtering=${FILTERING}&breakdowns=${BREAKDOWNS}&limit=500`
   const encodedUrl = encodeURI(baseUrl);  
 
   var response = UrlFetchApp.fetch(encodedUrl, {'method' : 'post'});  
@@ -46,15 +36,21 @@ function getFacebookAdsData() {
   const sheet = spreadSheet.getSheetByName(SHEET_TAB_NAME);
   sheet.clear();
 
-  const checkUrl = `https://graph.facebook.com/v10.0/${REPORT_ID}?access_token=${TOKEN}`;
+  const checkUrl = `https://graph.facebook.com/v14.0/${REPORT_ID}?access_token=${TOKEN}`;
   var checkAsync = UrlFetchApp.fetch(checkUrl);
   var checkResponse = JSON.parse(checkAsync.getContentText());
 
-  while (checkResponse.async_status != "Job Completed")
+  while (checkResponse.async_status != "Job Completed")   
   {
     Utilities.sleep(2000);
     checkAsync = UrlFetchApp.fetch(checkUrl);
     checkResponse = JSON.parse(checkAsync.getContentText());
+    
+    if(checkResponse.async_status == "Job Failed")
+    {
+      Logger.log(checkResponse.async_status);
+      return;
+    }
   }
   const reportUrl = `https://www.facebook.com/ads/ads_insights/export_report?report_run_id=${REPORT_ID}&access_token=${TOKEN}&format=csv`;
   const csv = UrlFetchApp.fetch(reportUrl);  
